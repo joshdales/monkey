7f06465431d1e0147ec4c36b13c9541a3419cc45
Josh Dales <git@joshdales.dev>
2025-12-12 19:08:26 +0000

    Add more keywords

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -5,7 +5,7 @@ import (
 	"monkey/token"
 	"testing"
 
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNextToken(t *testing.T) {
@@ -19,6 +19,12 @@ let add = fn(x, y) {
 let result = add(five, ten);
 !-/*5;
 5 < 10 > 5;
+
+if (5 < 10) {
+	return true;
+} else {
+	return false;
+}
 `
 
 	tests := []struct {
@@ -73,13 +79,30 @@ let result = add(five, ten);
 		{token.GT, ">"},
 		{token.INT, "5"},
 		{token.SEMICOLON, ";"},
+		{token.IF, "if"},
+		{token.LPAREN, "("},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
 		{token.EOF, ""},
 	}
 
 	lex := lexer.New(input)
 	for _, tt := range tests {
 		tok := lex.NextToken()
-		require.EqualValues(t, tt.expectedType, tok.Type)
-		require.EqualValues(t, tt.expectedLiteral, tok.Literal)
+		assert.Equalf(t, tt.expectedType, tok.Type, "Token.Type does not match")
+		assert.Equalf(t, tt.expectedLiteral, tok.Literal, "Token.Literal does not match")
 	}
 }
