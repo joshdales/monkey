78d79a1342316f67d283fc9ce6e39e76965caf64
Josh Dales <git@joshdales.dev>
2025-12-18 10:03:30 +0000

    Add precedences to the parser

---
+++ parser/precedences.go
@@ -0,0 +1,30 @@
+package parser
+
+import "monkey/token"
+
+var precedences = map[token.TokenType]int{
+	token.EQ:     EQUALS,
+	token.NOT_EQ: EQUALS,
+	token.LT:     LESSGREATER,
+	token.GT:     LESSGREATER,
+	token.PLUS:   SUM,
+	token.MINUS:  SUM,
+	token.SLASH:  PRODUCT,
+	token.ASSIGN: PRODUCT,
+}
+
+func (p *Parser) curPrecedence() int {
+	if pre, ok := precedences[p.curToken.Type]; ok {
+		return pre
+	}
+
+	return LOWEST
+}
+
+func (p *Parser) peekPrecedence() int {
+	if pre, ok := precedences[p.peekToken.Type]; ok {
+		return pre
+	}
+
+	return LOWEST
+}
