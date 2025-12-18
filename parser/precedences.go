a16127876e0abbab60b389e67397cb2653c1b4cf
Josh Dales <git@joshdales.dev>
2025-12-18 10:20:42 +0000

    Fix asterisk assignment

--- parser/precedences.go
+++ parser/precedences.go
@@ -3,14 +3,14 @@ package parser
 import "monkey/token"
 
 var precedences = map[token.TokenType]int{
-	token.EQ:     EQUALS,
-	token.NOT_EQ: EQUALS,
-	token.LT:     LESSGREATER,
-	token.GT:     LESSGREATER,
-	token.PLUS:   SUM,
-	token.MINUS:  SUM,
-	token.SLASH:  PRODUCT,
-	token.ASSIGN: PRODUCT,
+	token.EQ:       EQUALS,
+	token.NOT_EQ:   EQUALS,
+	token.LT:       LESSGREATER,
+	token.GT:       LESSGREATER,
+	token.PLUS:     SUM,
+	token.MINUS:    SUM,
+	token.SLASH:    PRODUCT,
+	token.ASTERISK: PRODUCT,
 }
 
 func (p *Parser) curPrecedence() int {
