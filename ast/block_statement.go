f77e29a139f5dc5f0b0bb44c0f22f38c5e7dd57f
Josh Dales <git@joshdales.dev>
2025-12-20 12:55:58 +0000

    Add if and block statements to the ast

---
+++ ast/block_statement.go
@@ -0,0 +1,29 @@
+package ast
+
+import (
+	"bytes"
+	"monkey/token"
+)
+
+type BlockStatement struct {
+	Token      token.Token // the `{` token
+	Statements []Statement
+}
+
+func (bs *BlockStatement) statementNode() {
+
+}
+
+func (bs *BlockStatement) TokenLiteral() string {
+	return bs.Token.Literal
+}
+
+func (bs *BlockStatement) String() string {
+	var out bytes.Buffer
+
+	for _, stmt := range bs.Statements {
+		out.WriteString(stmt.String())
+	}
+
+	return out.String()
+}
