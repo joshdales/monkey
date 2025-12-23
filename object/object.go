3ddc4420785c0afb894d76fe5ce2e08374448db7
Josh Dales <git@joshdales.dev>
2025-12-23 21:49:21 +0000

    Add struct for return value

--- object/object.go
+++ object/object.go
@@ -3,9 +3,10 @@ package object
 type ObjectType string
 
 const (
-	INTEGER_OBJ ObjectType = "INTEGER"
-	BOOLEAN_OBJ ObjectType = "BOOLEAN"
-	NULL_OBJ    ObjectType = "NULL"
+	INTEGER_OBJ      ObjectType = "INTEGER"
+	BOOLEAN_OBJ      ObjectType = "BOOLEAN"
+	NULL_OBJ         ObjectType = "NULL"
+	RETURN_VALUE_OBJ ObjectType = "RETURN_VALUE"
 )
 
 type Object interface {
