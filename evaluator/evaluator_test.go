9e9bf582c118c1a232c60642bc4b275830aa3bb3
Josh Dales <git@joshdales.dev>
2025-12-23 20:05:04 +0000

    Add boolena object tests

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -21,12 +21,29 @@ func TestEvalIntegerExpression(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		evaluated := testEval(tt.input)
+		evaluated := testEval(t, tt.input)
 		assertIntegerObject(t, tt.expected, evaluated)
 	}
 }
 
-func testEval(input string) object.Object {
+func TestEvalBooleanExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"true", true},
+		{"false", false},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(t, tt.input)
+		assertBooleanObject(t, tt.expected, evaluated)
+	}
+}
+
+func testEval(t *testing.T, input string) object.Object {
+	t.Helper()
+
 	l := lexer.New(input)
 	p := parser.New(l)
 	program := p.ParseProgram()
@@ -35,7 +52,17 @@ func testEval(input string) object.Object {
 }
 
 func assertIntegerObject(t *testing.T, expected int64, obj object.Object) {
+	t.Helper()
+
 	result, ok := obj.(*object.Integer)
 	require.Truef(t, ok, "object is not an Integer, got %T (%+v)", obj, obj)
+	assert.Equal(t, expected, result.Value)
+}
+
+func assertBooleanObject(t *testing.T, expected bool, obj object.Object) {
+	t.Helper()
+
+	result, ok := obj.(*object.Boolean)
+	require.Truef(t, ok, "object is not an Boolean, got %T (%+v)", obj, obj)
 	assert.Equal(t, expected, result.Value)
 }
