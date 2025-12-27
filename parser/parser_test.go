8e7245c57275ecd146dce7ae65df655db828f0e2
Josh Dales <git@joshdales.dev>
2025-12-27 18:51:13 +0000

    Add more tests

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -149,6 +149,8 @@ func TestOperatorPrecedenceParsing(t *testing.T) {
 		{"a + add(b * c) + d", "((a + add((b * c))) + d)"},
 		{"add(a, b, 1, 2 * 3, 4 + 5, add(6, 7 * 8))", "add(a, b, 1, (2 * 3), (4 + 5), add(6, (7 * 8)))"},
 		{"add(a + b + c * d / f + g)", "add((((a + b) + ((c * d) / f)) + g))"},
+		{"a * [1, 2, 3, 4][b *  c] * d", "((a * ([1, 2, 3, 4][(b * c)])) * d)"},
+		{"add(a * b[2], b[1], 2 * [1, 2][1])", "add((a * (b[2])), (b[1]), (2 * ([1, 2][1])))"},
 	}
 
 	for _, tt := range tests {
@@ -271,7 +273,7 @@ func TestArrayParsing(t *testing.T) {
 
 func TestIndexExpressionParsing(t *testing.T) {
 	input := "myArray[1 + 1]"
-	program := setupProgram(t, input, 1)
+	program := setupProgram(t, input, 0)
 	stmt := assertExpressionStatement(t, program.Statements[0])
 	idxExp, ok := stmt.Expression.(*ast.IndexExpression)
 	require.Truef(t, ok, "expected expression to be IndexExpression, got %T", stmt.Expression)
