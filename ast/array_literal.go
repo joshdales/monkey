19104da0def62fd6ac5cc5cae3e5ef504d919706
Josh Dales <git@joshdales.dev>
2025-12-27 18:20:52 +0000

    Add array literal to ast

---
+++ ast/array_literal.go
@@ -0,0 +1,29 @@
+package ast
+
+import (
+	"bytes"
+	"monkey/token"
+	"strings"
+)
+
+type ArrayLiteral struct {
+	Token    token.Token // the `token.LBRACKET` token
+	Elements []Expression
+}
+
+func (al *ArrayLiteral) expressionNode()      {}
+func (al *ArrayLiteral) TokenLiteral() string { return al.Token.Literal }
+func (al *ArrayLiteral) String() string {
+	var out bytes.Buffer
+
+	elements := make([]string, 0, len(al.Elements))
+	for _, el := range al.Elements {
+		elements = append(elements, el.String())
+	}
+
+	out.WriteString("[")
+	out.WriteString(strings.Join(elements, ", "))
+	out.WriteString("]")
+
+	return out.String()
+}
