6b7973a86899896ab29b7d21dc9d6b2dfd0e9012
Josh Dales <git@joshdales.dev>
2026-01-02 10:12:12 -0500

    Add Inspect method for the Hash

--- object/hash.go
+++ object/hash.go
@@ -1,5 +1,11 @@
 package object
 
+import (
+	"bytes"
+	"fmt"
+	"strings"
+)
+
 type HashKey struct {
 	Type  ObjectType
 	Value uint64
@@ -16,4 +22,19 @@ type Hash struct {
 
 func (h *Hash) Type() ObjectType {
 	return HASH_OBJ
+}
+
+func (h *Hash) Inspect() string {
+	var out bytes.Buffer
+
+	pairs := make([]string, len(h.Pairs))
+	for _, pair := range h.Pairs {
+		pairs = append(pairs, fmt.Sprintf("%s: %s", pair.Key.Inspect(), pair.Value.Inspect()))
+	}
+
+	out.WriteString("{")
+	out.WriteString(strings.Join(pairs, ", "))
+	out.WriteString("}")
+
+	return out.String()
 }
