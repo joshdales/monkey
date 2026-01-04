f13b3ac5c0c9f685e3606a65a5a5660dde79df03
Josh Dales <git@joshdales.dev>
2026-01-04 15:38:11 -0500

    Add lexer tests for macro

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -33,6 +33,7 @@ if (5 < 10) {
 "hello \"world\"";
 [1, 2];
 {"foo": "bar"};
+macro(x, y) { x + y; };
 `
 
 	tests := []struct {
@@ -128,6 +129,19 @@ if (5 < 10) {
 		{token.STRING, "foo"},
 		{token.COLON, ":"},
 		{token.STRING, "bar"},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.MACRO, "macro"},
+		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
 		{token.RBRACE, "}"},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
