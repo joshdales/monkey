7109776da52f8791b3287ada1d033b8206836c58
Josh Dales <git@joshdales.dev>
2026-01-04 13:38:44 -0500

    Add test desctiptions to modify

--- ast/modify_test.go
+++ ast/modify_test.go
@@ -26,10 +26,12 @@ func TestModify(t *testing.T) {
 	}
 
 	testCases := []struct {
+		desc     string
 		input    ast.Node
 		expected ast.Node
 	}{
 		{
+			"Program",
 			&ast.Program{
 				Statements: []ast.Statement{
 					&ast.ExpressionStatement{Expression: one()},
@@ -42,22 +44,27 @@ func TestModify(t *testing.T) {
 			},
 		},
 		{
+			"InfixExpression1",
 			&ast.InfixExpression{Left: one(), Operator: "+", Right: two()},
 			&ast.InfixExpression{Left: two(), Operator: "+", Right: two()},
 		},
 		{
+			"InfixExpression2",
 			&ast.InfixExpression{Left: two(), Operator: "+", Right: two()},
 			&ast.InfixExpression{Left: two(), Operator: "+", Right: two()},
 		},
 		{
+			"PrefixExpression1",
 			&ast.PrefixExpression{Operator: "-", Right: one()},
 			&ast.PrefixExpression{Operator: "-", Right: two()},
 		},
 		{
+			"PrefixExpression2",
 			&ast.IndexExpression{Left: one(), Index: one()},
 			&ast.IndexExpression{Left: two(), Index: two()},
 		},
 		{
+			"IfExpression",
 			&ast.IfExpression{
 				Condition: one(),
 				Consequence: &ast.BlockStatement{
@@ -78,14 +85,17 @@ func TestModify(t *testing.T) {
 			},
 		},
 		{
+			"ReturnStatement",
 			&ast.ReturnStatement{ReturnValue: one()},
 			&ast.ReturnStatement{ReturnValue: two()},
 		},
 		{
+			"LetStatement",
 			&ast.LetStatement{Value: one()},
 			&ast.LetStatement{Value: two()},
 		},
 		{
+			"FunctionLiteral",
 			&ast.FunctionLiteral{
 				Parameters: []*ast.Identifier{},
 				Body: &ast.BlockStatement{
@@ -104,13 +114,16 @@ func TestModify(t *testing.T) {
 			},
 		},
 		{
+			"ArrayLiteral",
 			&ast.ArrayLiteral{Elements: []ast.Expression{one(), two()}},
 			&ast.ArrayLiteral{Elements: []ast.Expression{two(), two()}},
 		},
 	}
 
 	for _, tC := range testCases {
-		modified := ast.Modify(tC.input, turnOneIntoTwo)
-		assert.ObjectsAreEqualValues(tC.expected, modified)
+		t.Run(tC.desc, func(t *testing.T) {
+			modified := ast.Modify(tC.input, turnOneIntoTwo)
+			assert.ObjectsAreEqualValues(tC.expected, modified)
+		})
 	}
 }
