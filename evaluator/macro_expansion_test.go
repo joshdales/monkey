52d59d464db68a02f4e9357de31bff0aff73b50a
Josh Dales <git@joshdales.dev>
2026-01-04 16:31:31 -0500

    Add macro expnsion test

---
+++ evaluator/macro_expansion_test.go
@@ -0,0 +1,35 @@
+package evaluator_test
+
+import (
+	"monkey/object"
+	"monkey/testutil"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDefineMacros(t *testing.T) {
+	input := `
+	let number = 1
+	let function = fn(x, y) {x + y};
+	let mymacro = macro(x, y) {x + y};
+	`
+	env := object.NewEnvironment()
+	program := testutil.SetupProgram(t, input, 3)
+
+	DefineMacros(env, program)
+
+	_, ok := env.Get("number")
+	assert.Falsef(t, ok, "number should not be defined")
+	_, ok = env.Get("function")
+	assert.Falsef(t, ok, "function should not be defined")
+	obj, ok := env.Get("mymacro")
+	assert.Truef(t, ok, "mymacro not in environment")
+	macro, ok := obj.(*object.Macro)
+	require.Truef(t, ok, "object is not a Macro, got %T (%+v)", obj, obj)
+	assert.Len(t, macro.Parameters, 2)
+	assert.Equal(t, "x", macro.Parameters[0].String())
+	assert.Equal(t, "y", macro.Parameters[1].String())
+	assert.Equal(t, "(x + y)", macro.Body.String())
+}
