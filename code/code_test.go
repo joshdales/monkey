b35663f708c95ffc79c2a09638aff7bcdc08567b
Josh Dales <git@joshdales.dev>
2026-01-18 14:55:26 -0500

    Add String helper for the instructions

--- code/code_test.go
+++ code/code_test.go
@@ -22,3 +22,22 @@ func TestMake(t *testing.T) {
 		assert.True(t, assert.ObjectsAreEqualValues(tt.expected, instruction))
 	}
 }
+
+func TestInstructionsString(t *testing.T) {
+	instructions := []code.Instructions{
+		code.Make(code.OpConstant, 1),
+		code.Make(code.OpConstant, 2),
+		code.Make(code.OpConstant, 65535),
+	}
+
+	expected := `0000 Opconstant 1
+0003 OpConstant 2
+0006 OpConstant 65535`
+
+	concatted := code.Instructions{}
+	for _, ins := range instructions {
+		concatted = append(concatted, ins...)
+	}
+
+	assert.Equal(t, expected, concatted.String(), "instructions wrongly formatted")
+}
