738878f5910efa35608a96ddbc5206c29c929212
Josh Dales <git@joshdales.dev>
2026-02-02 18:23:41 -0500

    Add logic for reading operands

--- code/code_test.go
+++ code/code_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestMake(t *testing.T) {
@@ -39,5 +40,30 @@ func TestInstructionsString(t *testing.T) {
 		concatted = append(concatted, ins...)
 	}
 
-	assert.Equal(t, expected, concatted.String(), "instructions wrongly formatted")
+	assert.Equalf(t,
+		expected,
+		concatted.String(),
+		"instructions wrongly formatted.\nwant %q\ngot %q", expected, concatted.String(),
+	)
+}
+
+func TestReadOperands(t *testing.T) {
+	tests := []struct {
+		op        code.Opcode
+		operands  []int
+		bytesRead int
+	}{
+		{code.OpConstant, []int{65535}, 2},
+	}
+
+	for _, tt := range tests {
+		instruction := code.Make(tt.op, tt.operands...)
+		def, err := code.Lookup(byte(tt.op))
+		require.NoErrorf(t, err, "definition not found: %q", err)
+		operandsRead, n := code.ReadOperands(def, instruction[1:])
+		assert.Equal(t, tt.bytesRead, n)
+		for i, want := range tt.operands {
+			assert.Equal(t, want, operandsRead[i])
+		}
+	}
 }
