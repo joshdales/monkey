4e6cd814d3b4be290c8a1bc26d3a8a57f234fb9b
Josh Dales <git@joshdales.dev>
2026-02-05 09:16:22 -0500

    Update compiler to add

--- compiler/compiler_test.go
+++ compiler/compiler_test.go
@@ -15,10 +15,12 @@ type compilerTestCase struct {
 func TestIntegerArithmetic(t *testing.T) {
 	tests := []compilerTestCase{
 		{
-			input: "1 + 2", expectedConstants: []any{1, 2},
+			input:             "1 + 2",
+			expectedConstants: []any{1, 2},
 			expectedInstructions: []code.Instructions{
 				code.Make(code.OpConstant, 0),
 				code.Make(code.OpConstant, 1),
+				code.Make(code.OpAdd),
 			},
 		},
 	}
