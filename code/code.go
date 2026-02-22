bea3827744c1dc4c6a81cfd4809cef20a7af09fd
Josh Dales <git@joshdales.dev>
2026-02-22 14:22:05 -0500

    Index expressions in the compiler

--- code/code.go
+++ code/code.go
@@ -28,6 +28,7 @@ const (
 	OpSetGlobal
 	OpArray
 	OpHash
+	OpIndex
 )
 
 type Definition struct {
@@ -56,6 +57,7 @@ var definitions = map[Opcode]*Definition{
 	OpSetGlobal:     {"OpSetGlobal", []int{2}},
 	OpArray:         {"OpArray", []int{2}},
 	OpHash:          {"OpHash", []int{2}},
+	OpIndex:         {"OpIndex", []int{}},
 }
 
 func Lookup(op byte) (*Definition, error) {
