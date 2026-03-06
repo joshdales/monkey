39bd489ff26e3a21ea3007c69d6f4f50d61f082e
Josh Dales <git@joshdales.dev>
2026-03-06 14:05:37 -0500

    Add a compiled function object

--- object/object.go
+++ object/object.go
@@ -3,18 +3,19 @@ package object
 type ObjectType string
 
 const (
-	INTEGER_OBJ      ObjectType = "INTEGER"
-	BOOLEAN_OBJ      ObjectType = "BOOLEAN"
-	NULL_OBJ         ObjectType = "NULL"
-	RETURN_VALUE_OBJ ObjectType = "RETURN_VALUE"
-	ERROR_OBJ        ObjectType = "ERROR"
-	FUNCTION_OBJ     ObjectType = "FUNCTION"
-	STRING_OBJ       ObjectType = "STRING"
-	BUILTIN_OBJ      ObjectType = "BUILTIN"
-	ARRAY_OBJ        ObjectType = "ARRAY"
-	HASH_OBJ         ObjectType = "HASH"
-	QUOTE_OBJ        ObjectType = "QUOTE"
-	MACRO_OBJ        ObjectType = "MACRO"
+	INTEGER_OBJ           ObjectType = "INTEGER"
+	BOOLEAN_OBJ           ObjectType = "BOOLEAN"
+	NULL_OBJ              ObjectType = "NULL"
+	RETURN_VALUE_OBJ      ObjectType = "RETURN_VALUE"
+	ERROR_OBJ             ObjectType = "ERROR"
+	FUNCTION_OBJ          ObjectType = "FUNCTION"
+	COMPILED_FUNCTION_OBJ ObjectType = "COMPILED_FUNCTION_OBJ"
+	STRING_OBJ            ObjectType = "STRING"
+	BUILTIN_OBJ           ObjectType = "BUILTIN"
+	ARRAY_OBJ             ObjectType = "ARRAY"
+	HASH_OBJ              ObjectType = "HASH"
+	QUOTE_OBJ             ObjectType = "QUOTE"
+	MACRO_OBJ             ObjectType = "MACRO"
 )
 
 type Object interface {
