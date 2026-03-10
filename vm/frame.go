4b8ddc8b657cd59c0a0964a1d1dc84d1feb17fc0
Josh Dales <git@joshdales.dev>
2026-03-10 13:36:39 -0400

    Store the point in a frame

--- vm/frame.go
+++ vm/frame.go
@@ -7,12 +7,13 @@ import (
 
 // A Frame holds execution-relevant information.
 type Frame struct {
-	fn *object.CompiledFunction // Compiled function referenced by this Frame
-	ip int                      // Instruction pointer in this Frame, for this Frame
+	fn          *object.CompiledFunction // Compiled function referenced by this Frame
+	ip          int                      // Instruction pointer in this Frame, for this Frame
+	basepointer int                      // The pointer value before the function was executed
 }
 
-func NewFrame(fn *object.CompiledFunction) *Frame {
-	return &Frame{fn: fn, ip: -1}
+func NewFrame(fn *object.CompiledFunction, basePointer int) *Frame {
+	return &Frame{fn: fn, ip: -1, basepointer: basePointer}
 }
 
 func (f *Frame) Instructions() code.Instructions {
--- vm/vm.go
+++ vm/vm.go
@@ -34,7 +34,7 @@ type VM struct {
 
 func New(bytecode *compiler.Bytecode) *VM {
 	mainFn := &object.CompiledFunction{Instructions: bytecode.Instructions}
-	mainFrame := NewFrame(mainFn)
+	mainFrame := NewFrame(mainFn, 0)
 
 	frames := make([]*Frame, MaxFrames)
 	frames[0] = mainFrame
@@ -193,7 +193,7 @@ func (vm *VM) Run() error {
 			if !ok {
 				return fmt.Errorf("calling non-function")
 			}
-			frame := NewFrame(fn)
+			frame := NewFrame(fn, vm.sp)
 			vm.pushFrame(frame)
 
 		case code.OpReturnValue:
