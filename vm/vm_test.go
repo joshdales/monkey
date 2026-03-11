849b26b780c2661b05942c26df532ad99f395993
Josh Dales <git@joshdales.dev>
2026-03-11 15:35:18 -0400

    Add more vm tests

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -220,6 +220,18 @@ func TestCallingFunctionsWithArgumentsAndBindings(t *testing.T) {
 			input:    `let sum = fn(a, b) { a + b; }; sum(1, 2);`,
 			expected: 3,
 		},
+		{
+			input:    `let sum = fn(a, b) { let c = a + b; c; }; sum(1, 2);`,
+			expected: 3,
+		},
+		{
+			input:    `let sum = fn(a, b) { let c = a + b; c; }; sum(1, 2) + sum(3, 4);`,
+			expected: 10,
+		},
+		{
+			input:    `let sum = fn(a, b) { let c = a + b; c; }; let outer = fn() { sum(1, 2) + sum(3, 4); }; outer();`,
+			expected: 10,
+		},
 	}
 
 	runVmTest(t, tests)
