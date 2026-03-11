9c692db8d30ea94151b2f4d805b87da185982d5b
Josh Dales <git@joshdales.dev>
2026-03-11 13:58:17 -0400

    Add tests for checking function arguments

--- compiler/compiler_test.go
+++ compiler/compiler_test.go
@@ -566,6 +566,53 @@ func TestFunctionCalls(t *testing.T) {
 				code.Make(code.OpPop),
 			},
 		},
+		{
+			input: `let oneArg = fn(a) { }; oneArg(24);`,
+			expectedConstants: []any{
+				[]code.Instructions{code.Make(code.OpReturn)},
+				24,
+			},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpConstant, 0),
+				code.Make(code.OpSetGlobal, 0),
+				code.Make(code.OpGetGlobal, 0),
+				code.Make(code.OpConstant, 1),
+				code.Make(code.OpCall, 1),
+				code.Make(code.OpPop),
+			},
+		},
+		{
+			input: `let oneArg = fn(a) { }; oneArg(24);`,
+			expectedConstants: []any{
+				[]code.Instructions{code.Make(code.OpReturn)},
+				24,
+			},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpConstant, 0),
+				code.Make(code.OpSetGlobal, 0),
+				code.Make(code.OpGetGlobal, 0),
+				code.Make(code.OpConstant, 1),
+				code.Make(code.OpCall, 1),
+				code.Make(code.OpPop),
+			},
+		},
+		{
+			input: `let manyArgs = fn(a, b, c) { }; manyArgs(24, 25, 26);`,
+			expectedConstants: []any{
+				[]code.Instructions{code.Make(code.OpReturn)},
+				24, 25, 26,
+			},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpConstant, 0),
+				code.Make(code.OpSetGlobal, 0),
+				code.Make(code.OpGetGlobal, 0),
+				code.Make(code.OpConstant, 1),
+				code.Make(code.OpConstant, 2),
+				code.Make(code.OpConstant, 3),
+				code.Make(code.OpCall, 3),
+				code.Make(code.OpPop),
+			},
+		},
 	}
 
 	runCompilerTests(t, tests)
