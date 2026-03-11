90d1991040d5fd80d6b6e2053961fa78f8a3ade3
Josh Dales <git@joshdales.dev>
2026-03-11 14:03:13 -0400

    Add compiler handling for the function arguments

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -292,7 +292,14 @@ func (c *Compiler) Compile(node ast.Node) error {
 			return err
 		}
 
-		c.emit(code.OpCall)
+		for _, arg := range node.Arguments {
+			err := c.Compile(arg)
+			if err != nil {
+				return nil
+			}
+		}
+
+		c.emit(code.OpCall, len(node.Arguments))
 	}
 
 	return nil
