764b61b139e6660c79238ab8a8ad4678ba6376d2
Josh Dales <git@joshdales.dev>
2026-03-13 18:04:48 -0400

    Allow repl to use interpreter or compiler

--- repl/repl.go
+++ repl/repl.go
@@ -4,16 +4,47 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"os"
+	"strconv"
+
 	"monkey/compiler"
+	"monkey/evaluator"
 	"monkey/lexer"
 	"monkey/object"
 	"monkey/parser"
 	"monkey/vm"
 )
 
+var compilerEnabled bool
+
+func checkCompilerEnabled() {
+	noCompiler, err := strconv.ParseBool(os.Getenv("DISABLE_COMPILER"))
+	if err != nil {
+		compilerEnabled = true
+	}
+
+	compilerEnabled = !noCompiler
+}
+
 const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
+	checkCompilerEnabled()
+	if compilerEnabled {
+		compilerStart(in, out)
+	} else {
+		interpreterStart(in, out)
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, "Parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
+}
+
+func compilerStart(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 	constants := []object.Object{}
 	globals := make([]object.Object, vm.GlobalsSize)
@@ -56,20 +87,36 @@ func Start(in io.Reader, out io.Writer) {
 		lastPopped := machine.LastPoppedStackElem()
 		io.WriteString(out, lastPopped.Inspect())
 		io.WriteString(out, "\n")
-
-		// evaluator.DefineMacros(macroEnv, program)
-		// expanded := evaluator.ExpandMacros(macroEnv, program)
-		// evaluated := evaluator.Eval(env, expanded)
-		// if evaluated != nil {
-		// 	io.WriteString(out, evaluated.Inspect())
-		// 	io.WriteString(out, "\n")
-		// }
 	}
 }
 
-func printParserErrors(out io.Writer, errors []string) {
-	io.WriteString(out, "Parser errors:\n")
-	for _, msg := range errors {
-		io.WriteString(out, "\t"+msg+"\n")
+func interpreterStart(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
+	macroEnv := object.NewEnvironment()
+
+	for {
+		fmt.Fprint(out, PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		if len(p.Errors()) > 0 {
+			printParserErrors(out, p.Errors())
+			continue
+		}
+
+		evaluator.DefineMacros(macroEnv, program)
+		expanded := evaluator.ExpandMacros(macroEnv, program)
+		evaluated := evaluator.Eval(env, expanded)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
