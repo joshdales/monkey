86c60c87ece466831b4cc1800869f33571e67ea1
Josh Dales <git@joshdales.dev>
2026-03-14 18:27:02 -0400

    Update vm frame to use closures

--- vm/frame.go
+++ vm/frame.go
@@ -7,15 +7,15 @@ import (
 
 // A Frame holds execution-relevant information.
 type Frame struct {
-	fn          *object.CompiledFunction // Compiled function referenced by this Frame
-	ip          int                      // Instruction pointer in this Frame, for this Frame
-	basepointer int                      // The pointer value before the function was executed
+	cl          *object.Closure // Closure for compiled function referenced by this Frame
+	ip          int             // Instruction pointer in this Frame, for this Frame
+	basepointer int             // The pointer value before the function was executed
 }
 
-func NewFrame(fn *object.CompiledFunction, basePointer int) *Frame {
-	return &Frame{fn: fn, ip: -1, basepointer: basePointer}
+func NewFrame(cl *object.Closure, basePointer int) *Frame {
+	return &Frame{cl: cl, ip: -1, basepointer: basePointer}
 }
 
 func (f *Frame) Instructions() code.Instructions {
-	return f.fn.Instructions
+	return f.cl.Fn.Instructions
 }
