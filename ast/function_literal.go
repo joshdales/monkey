4f2823f52f24a686a756b680ca23da0a797bcf8d
Josh Dales <git@joshdales.dev>
2026-03-14 21:27:59 -0400

    Add the name of the function to the ast

--- ast/function_literal.go
+++ ast/function_literal.go
@@ -2,6 +2,7 @@ package ast
 
 import (
 	"bytes"
+	"fmt"
 	"monkey/token"
 	"strings"
 )
@@ -10,6 +11,7 @@ type FunctionLiteral struct {
 	Token      token.Token // The `token.FUNCTION` token
 	Parameters []*Identifier
 	Body       *BlockStatement
+	Name       string
 }
 
 func (fl *FunctionLiteral) expressionNode() {
@@ -29,6 +31,9 @@ func (fl *FunctionLiteral) String() string {
 	}
 
 	out.WriteString(fl.TokenLiteral())
+	if fl.Name != "" {
+		out.WriteString(fmt.Sprintf("<%s>", fl.Name))
+	}
 	out.WriteString("(")
 	out.WriteString(strings.Join(params, ", "))
 	out.WriteString(") ")
