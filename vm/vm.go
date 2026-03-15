9422d0cd68483fe81605a6baf3caca8d84661537
Josh Dales <git@joshdales.dev>
2026-03-14 21:06:27 -0400

    Upate error message

--- vm/vm.go
+++ vm/vm.go
@@ -521,7 +521,7 @@ func (vm *VM) executeCall(numArgs int) error {
 	case *object.Builtin:
 		return vm.callBuiltin(callee, numArgs)
 	default:
-		return fmt.Errorf("calling non-function and non-built-in")
+		return fmt.Errorf("calling non-closure and non-built-in")
 	}
 }
 
