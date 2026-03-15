7d3bb33942d918e6dd9516ec2965054414385c6a
Josh Dales <git@joshdales.dev>
2026-03-14 21:47:20 -0400

    Move Resolve

--- compiler/symbol_table.go
+++ compiler/symbol_table.go
@@ -49,24 +49,6 @@ func (s *SymbolTable) Define(name string) Symbol {
 	return symbol
 }
 
-func (s *SymbolTable) Resolve(name string) (Symbol, bool) {
-	obj, ok := s.store[name]
-	if !ok && s.Outer != nil {
-		obj, ok = s.Outer.Resolve(name)
-		if !ok {
-			return obj, ok
-		}
-
-		if obj.Scope == GlobalScope || obj.Scope == BuiltinScope {
-			return obj, ok
-		}
-
-		free := s.defineFree(obj)
-		return free, true
-	}
-	return obj, ok
-}
-
 func (s *SymbolTable) DefineBuiltin(index int, name string) Symbol {
 	symbol := Symbol{Name: name, Index: index, Scope: BuiltinScope}
 	s.store[name] = symbol
@@ -85,4 +67,22 @@ func (s *SymbolTable) defineFree(original Symbol) Symbol {
 	symbol.Scope = FreeScope
 	s.store[original.Name] = symbol
 	return symbol
+}
+
+func (s *SymbolTable) Resolve(name string) (Symbol, bool) {
+	obj, ok := s.store[name]
+	if !ok && s.Outer != nil {
+		obj, ok = s.Outer.Resolve(name)
+		if !ok {
+			return obj, ok
+		}
+
+		if obj.Scope == GlobalScope || obj.Scope == BuiltinScope {
+			return obj, ok
+		}
+
+		free := s.defineFree(obj)
+		return free, true
+	}
+	return obj, ok
 }
